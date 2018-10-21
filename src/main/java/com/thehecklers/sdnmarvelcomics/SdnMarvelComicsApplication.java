10a7fb05a13ea2e7935b0f47bbba98da209b2560
Mark Heckler <mark.heckler@gmail.com>
2018-10-21 12:07:49 -0700

    Initial commit

---
+++ src/main/java/com/thehecklers/sdnmarvelcomics/SdnMarvelComicsApplication.java
@@ -0,0 +1,12 @@
+package com.thehecklers.sdnmarvelcomics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdnMarvelComicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdnMarvelComicsApplication.class, args);
+	}
+}
